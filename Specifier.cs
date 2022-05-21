7f80a2ceb6df3a00f78e3a4831983ab88ea8447e
Igor Karatayev <igor.cwer@gmail.com>
2022-05-21 21:20:01 +0300

    update

---
+++ Specifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UE4Assistant.Specifier
+{
+	public record struct A();
+
+	public struct Specifier
+	{
+		public int startIndex;
+		public int endIndex;
+		public string type;
+		public TagModel tag;
+		public Dictionary<string, object> data;
+
+		public override string ToString()
+		{
+			return type + data.GenerateSpecifier(SpecifierSchema.ReadSpecifierSettings(type));
+		}
+	}
+
+}
